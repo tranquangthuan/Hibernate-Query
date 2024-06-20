0373850fb3e8207eda5c1f505ce725ca65f7e551
Thuan Tran <thuan.uit@gmail.com>
2024-06-20 22:06:14 +0700

    update validation

--- src/main/java/com/thuan/hibernate/main/validation/TestValidation.java
+++ src/main/java/com/thuan/hibernate/main/validation/TestValidation.java
@@ -1,7 +1,6 @@
 package com.thuan.hibernate.main.validation;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -9,8 +8,6 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
-import com.thuan.hibernate.entity.Car;
-import com.thuan.hibernate.entity.Person;
 import com.thuan.hibernate.entity.User;
 
 public class TestValidation {
@@ -23,7 +20,7 @@ public class TestValidation {
 
 		User user = new User(1, "A", 210, "@gmail.com", LocalDate.of(2022, 1, 1));
 		User validUser = new User(1, "ABC", 20, "thuan@gmail.com", LocalDate.now(), "0982992593");
-		Set<ConstraintViolation<User>> constraintViolations = validator.validate(validUser);
+		Set<ConstraintViolation<User>> constraintViolations = validator.validate(user);
 		// Show errors
 		if (constraintViolations.size() > 0) {
 			for (ConstraintViolation<User> violation : constraintViolations) {
